ea3a9a322b5e5822fbb101f4eef973ed83f39e70
João Pedro <joaope.espindola2005@gmail.com>
2024-08-21 16:56:51 -0300

    theme

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles"
+
+export default function Details(){
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    BACKGROUND_700: "#262529",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_900: "0D0C0F",
+
+    PINK: "FF859B",
+    PURPLE: "948F99",
+    BROWN: "282124"
+  }
+}
