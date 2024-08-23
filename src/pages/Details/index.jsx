8e751bbcfbecc4c1b67f1c8c37c4a3d9708585db
João Pedro <joaope.espindola2005@gmail.com>
2024-08-23 14:17:39 -0300

    component ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,14 @@
 import { Container } from "./styles"
 
 import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
 import { Tags } from "../../components/Tags"
 
 export default function Details(){
   return(
     <Container>
       <Header></Header>
+      <ButtonText title="Voltar"/>
       <Tags title ="Ficção Científica"/>
     </Container>
   )
