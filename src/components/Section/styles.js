fbb2bed96b10e97fa538820e70e7e8ffcf28d12a
João Pedro <joaope.espindola2005@gmail.com>
2024-08-23 14:23:17 -0300

    component Section

---
+++ src/components/Section/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+ margin: 24px 0;
+ > h2 {
+    font-size: 36px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    margin-bottom: 8px;
+ }
+`
