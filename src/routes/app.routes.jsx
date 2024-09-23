0dccc6add122cc1b8ba83f3e1d8497784d93a748
João Pedro <joaope.espindola2005@gmail.com>
2024-09-23 12:01:00 -0300

    Handling Unavailable Routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { New } from '../pages/New'
 import { Home } from '../pages/Home'
 import { Profile } from '../pages/Profile'
-import  Details from '../pages/Details'
+import Details from '../pages/Details'
 
 export function AppRoutes(){
   return(
@@ -12,6 +12,8 @@ export function AppRoutes(){
       <Route path="/new" element={<New />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/details/:id" element={<Details />} />
+
+      <Route path='*' element={ <Navigate to="/"/> }/>
     </Routes>
   )
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { SignUp } from '../pages/SignUp'
 import { SignIn } from '../pages/SignIn'
 
 export function AuthRoutes(){
+  const user = localStorage.getItem("@rocketnotes:user")
   return(
     <Routes>
+      
       <Route path="/" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
+
+      { !user && <Route path='*' element={ <Navigate to="/"/> }/>}
     </Routes>
   )
 }
